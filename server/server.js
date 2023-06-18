b25040e04c84904dfc6f5a2381d05812155ed556
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-18 14:07:53 -0300

    se crearon las peticiones de usuarios con Postman.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 import express from 'express';
+
+import { config } from './config/config.js';
 const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello, world');
+});
+app.get('/usuarios', function (req, res) {
+  res.json('GET usuarios');
+});
+
+app.post('/usuarios', function (req, res) {
+  res.json('POST usuarios');
+});
+
+app.put('/usuarios/:id', function (req, res) {
+  res.json('PUT usuarios');
+});
+
+app.delete('/usuarios/:id', function (req, res) {
+  res.json('DELETE usuarios');
 });
 
 app.listen(4000, () => {
